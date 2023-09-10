BopDataset objects loading/cropping fixes. Renderer projection matrix calculation based on scene camera matrix. Multiporcess maps rendering.

sdp/ds/bop_dataset.py
@@ -17,12 +17,20 @@ from sdp.ds.batch_processor import BatchProcessor
 from sdp.ds.bop_data import read_scene_camera, read_scene_gt, read_scene_gt_info, id_to_str
 from sdp.utils.data import read_yaml, write_yaml
 
+MAP_NORM_NAME = 'norm'
+MAP_NOC_NAME = 'noc'
 SplitsType = Union[int, list[int], float, list[float]]
-IdsType = Union[int, list[int], np.ndarray, range]
+IndsList = Union[list[int], np.ndarray, slice]
+IdsType = Union[int, IndsList]
+ImgsList = list[np.ndarray]
+MapsDict = dict[str, ImgsList]
 
 
-def get_img_fpath(scene_path: Path, img_id_str: str) -> Path:
-    return scene_path / 'rgb' / f'{img_id_str}.jpg'
+def get_img_fpath(scene_path: Path, img_id_str: str, dir_sfx: str = '', ext='jpg') -> Path:
+    dir_name = 'rgb'
+    if dir_sfx:
+        dir_name = f'{dir_name}_{dir_sfx}'
+    return scene_path / dir_name / f'{img_id_str}.{ext}'
 
 
 def get_mask_visib_fpath(scene_path: Path, img_id_str: str, obj_ind: int) -> Path:
@@ -89,82 +97,116 @@ def split_range(n: int, splits: SplitsType) -> list[int]:
 
 
 class ImgsMasksCrop:
-    imgs_crop: list[np.ndarray]
-    masks_crop: list[np.ndarray]
+    imgs_crop: ImgsList
+    masks_crop: ImgsList
+    maps_crop: MapsDict
     bboxes_ltwh_src: list[np.ndarray]
     ratios_src_to_dst: list[float]
 
-    def __init__(self, imgs_crop: list[np.ndarray], masks_crop: list[np.ndarray],
+    def __init__(self, imgs_crop: ImgsList, masks_crop: ImgsList, maps_crop: MapsDict,
                  bboxes_ltwh_src: list[np.ndarray], ratios_src_to_dst: list[float]):
         self.imgs_crop = imgs_crop
         self.masks_crop = masks_crop
+        self.maps_crop = maps_crop
         self.bboxes_ltwh_src = bboxes_ltwh_src
         self.ratios_src_to_dst = ratios_src_to_dst
 
 
-def crop_apply_masks(imgs: list[np.ndarray], masks: list[np.ndarray], bboxes_ltwh: list[np.ndarray], patch_sz: int,
-                     offset: float = 0.05) -> ImgsMasksCrop:
-    n = len(imgs)
-    imgs_crop = [np.zeros((patch_sz, patch_sz, 3), dtype=np.uint8) for _ in range(n)]
-    masks_crop = [np.zeros((patch_sz, patch_sz), dtype=np.uint8) for _ in range(n)]
-    bboxes_ltwh_src = [np.zeros(4, dtype=float) for _ in range(n)]
-    ratios_src_to_dst = []
-    r = 1 + offset
-    for img, mask, bbox_ltwh, img_crop, mask_crop, bbox_ltwh_src in zip(imgs, masks, bboxes_ltwh, imgs_crop, masks_crop, bboxes_ltwh_src):
-        img_sz = np.array((img.shape[1], img.shape[0]))
-        bbox_center = bbox_ltwh[:2] + bbox_ltwh[2:] / 2
-        bbox_sz = np.max(bbox_ltwh[2:])
-        bbox_sz_ext = bbox_sz * r
-        bbox_sz_ext2 = bbox_sz_ext / 2
-        bbox_sz_ext = bbox_sz_ext.astype(int)
-        bb_lt = (bbox_center - bbox_sz_ext2).astype(int)
-        bb_rb = bb_lt + bbox_sz_ext - 1
-        bb_lt_fit, bb_rb_fit = np.maximum(bb_lt, 0), np.minimum(bb_rb, img_sz - 1)
-        bb_sz_fit = bb_rb_fit - bb_lt_fit + 1
-        bb_lt_off = bb_lt_fit - bb_lt
-        img_patch = np.zeros((bbox_sz_ext, bbox_sz_ext, 3), np.uint8)
-        mask_patch = np.zeros((bbox_sz_ext, bbox_sz_ext), np.uint8)
-        p_p1, p_p2 = bb_lt_off, bb_lt_off + bb_sz_fit
-        i_p1, i_p2 = bb_lt_fit, bb_lt_fit + bb_sz_fit
-        slices_patch = slice(p_p1[1], p_p2[1]), slice(p_p1[0], p_p2[0])
-        slices_img = slice(i_p1[1], i_p2[1]), slice(i_p1[0], i_p2[0])
-        img_patch[slices_patch] = img[slices_img]
-        mask_patch[slices_patch] = mask[slices_img]
-        cv2.resize(img_patch, img_crop.shape[:2], img_crop, interpolation=cv2.INTER_AREA)
-        cv2.resize(mask_patch, mask_crop.shape, mask_crop, interpolation=cv2.INTER_NEAREST)
-        bbox_ltwh_src[:2] = bb_lt_fit
-        bbox_ltwh_src[2:] = bb_sz_fit
-        ratios_src_to_dst.append(patch_sz / bbox_sz_ext)
-    return ImgsMasksCrop(imgs_crop, masks_crop, bboxes_ltwh_src, ratios_src_to_dst)
+def make_square_img(sz: int, ch: int) -> np.ndarray:
+    shape = (sz, sz) if ch == 1 else (sz, sz, ch)
+    return np.zeros(shape, dtype=np.uint8)
+
+
+def crop_apply_mask(imgs: list[np.ndarray], mask: np.ndarray, bbox_ltwh: np.ndarray, sz_out: int,
+                    bb_ext_offset: float = 0.05) -> tuple[list[np.ndarray], np.ndarray, np.ndarray, float]:
+    imgs_out = [make_square_img(sz_out, 3) for _ in imgs]
+    mask_out = make_square_img(sz_out, 1)
+    r = 1 + bb_ext_offset
+    sz_out2 = sz_out, sz_out
+    img_sz = np.array((imgs[0].shape[1], imgs[0].shape[0]))
+    bbox_center = bbox_ltwh[:2] + bbox_ltwh[2:] / 2
+    bbox_sz = np.max(bbox_ltwh[2:])
+    bbox_sz_ext = bbox_sz * r
+    bbox_sz_ext2 = bbox_sz_ext / 2
+    bbox_sz_ext = bbox_sz_ext.astype(int)
+    bb_lt = (bbox_center - bbox_sz_ext2).astype(int)
+    bb_rb = bb_lt + bbox_sz_ext - 1
+    bb_lt_fit, bb_rb_fit = np.maximum(bb_lt, 0), np.minimum(bb_rb, img_sz - 1)
+    bb_sz_fit = bb_rb_fit - bb_lt_fit + 1
+    bb_lt_off = bb_lt_fit - bb_lt
+    img_patch = np.zeros((bbox_sz_ext, bbox_sz_ext, 3), np.uint8)
+    mask_patch = np.zeros((bbox_sz_ext, bbox_sz_ext), np.uint8)
+    p_p1, p_p2 = bb_lt_off, bb_lt_off + bb_sz_fit
+    i_p1, i_p2 = bb_lt_fit, bb_lt_fit + bb_sz_fit
+    slices_patch = slice(p_p1[1], p_p2[1]), slice(p_p1[0], p_p2[0])
+    slices_img = slice(i_p1[1], i_p2[1]), slice(i_p1[0], i_p2[0])
+
+    for i in range(len(imgs)):
+        if i > 0:
+            img_patch.fill(0)
+        img_patch[slices_patch] = imgs[i][slices_img]
+        cv2.resize(img_patch, sz_out2, imgs_out[i], interpolation=cv2.INTER_AREA)
+
+    mask_patch[slices_patch] = mask[slices_img]
+    cv2.resize(mask_patch, sz_out2, mask_out, interpolation=cv2.INTER_NEAREST)
+
+    bbox_ltwh_src = np.concatenate([bb_lt_fit, bb_sz_fit])
+    ratio_src_to_dst = sz_out / bbox_sz_ext
+
+    return imgs_out, mask_out, bbox_ltwh_src, ratio_src_to_dst
 
 
 class GtImgsMasks:
     df_img: pd.DataFrame
     df_obj: pd.DataFrame
-    imgs: list[np.ndarray]
-    masks: list[np.ndarray]
-    imgs_crop: list[np.ndarray]
-    masks_crop: list[np.ndarray]
+    imgs: ImgsList
+    masks: ImgsList
+    maps: MapsDict
+    imgs_crop: ImgsList
+    masks_crop: ImgsList
+    maps_crop: MapsDict
     bboxes_ltwh_src: list[np.ndarray]
     ratios_src_to_dst: list[float]
 
     def __init__(self, df_img: pd.DataFrame, df_obj: pd.DataFrame,
-                 imgs: list[np.ndarray], masks: list[np.ndarray]):
+                 imgs: ImgsList, masks: ImgsList, maps: MapsDict):
         self.df_img = df_img
         self.df_obj = df_obj
         self.imgs = imgs
         self.masks = masks
+        self.maps = maps
         self.imgs_crop = []
         self.masks_crop = []
+        self.maps_crop = {}
         self.bboxes_ltwh_src = []
         self.ratios_src_to_dst = []
 
     def crop(self, patch_sz: int, offset: float = 0.05):
-        img_masks_crop = crop_apply_masks(self.imgs, self.masks, self.df_obj['bbox_visib_ltwh'], patch_sz, offset)
-        self.imgs_crop = img_masks_crop.imgs_crop
-        self.masks_crop = img_masks_crop.masks_crop
-        self.bboxes_ltwh_src = img_masks_crop.bboxes_ltwh_src
-        self.ratios_src_to_dst = img_masks_crop.ratios_src_to_dst
+        img_id_to_ind = {self.df_img.index[i]: i for i in range(len(self.df_img))}
+        map_names = list(self.maps.keys())
+        imgs_crop = []
+        maps_crop = {name: [] for name in map_names}
+        masks_crop = []
+        bboxes_ltwh_src = []
+        ratios_src_to_dst = []
+        for _, orow in self.df_obj.iterrows():
+            img_ind = img_id_to_ind[orow['img_ds_id']]
+            img = self.imgs[img_ind]
+            maps = [self.maps[name][img_ind] for name in map_names]
+            imgs = [img, *maps]
+            imgs_out, mask_out, bbox_ltwh_src, ratio_src_to_dst = \
+                crop_apply_mask(imgs, self.masks[img_ind], orow['bbox_visib_ltwh'], patch_sz, offset)
+            imgs_crop.append(imgs_out[0])
+            for i, name in enumerate(map_names):
+                maps_crop[name].append(imgs_out[i + 1])
+            masks_crop.append(mask_out)
+            bboxes_ltwh_src.append(bbox_ltwh_src)
+            ratios_src_to_dst.append(ratio_src_to_dst)
+        self.imgs_crop = imgs_crop
+        self.masks_crop = masks_crop
+        self.maps_crop = maps_crop
+        self.bboxes_ltwh_src = bboxes_ltwh_src
+        self.ratios_src_to_dst = ratios_src_to_dst
 
 
 def create_aug_default() -> iaa.Augmenter:
@@ -199,20 +241,23 @@ class ImgsObjsGtParams:
     ds_path: Path
     irows: pd.DataFrame
     orows: pd.DataFrame
+    maps_names: list[str]
     out_size: Optional[int] = None
     aug_name: Optional[str] = None
 
     def __init__(self, ds_path: Path, irows: pd.DataFrame, orows: pd.DataFrame, out_size: Optional[int] = None,
-                 aug_name: Optional[str] = None):
+                 aug_name: Optional[str] = None, maps_names: Optional[list[str]] = None):
         self.ds_path = ds_path
         self.irows = irows.copy()
         self.orows = orows.copy()
         self.out_size = out_size
         self.aug_name = aug_name
+        self.maps_names = [] if maps_names is None else maps_names
 
 
 def load_imgs_objs_gt(params: ImgsObjsGtParams) -> GtImgsMasks:
     imgs, masks = [], []
+    maps = {map_name: [] for map_name in params.maps_names}
     for img_ds_id, irow in params.irows.iterrows():
         orows_img = params.orows[params.orows['img_ds_id'] == img_ds_id]
         scene_id_str = id_to_str(irow['scene_id'])
@@ -232,11 +277,16 @@ def load_imgs_objs_gt(params: ImgsObjsGtParams) -> GtImgsMasks:
             mask_img[mask > 0] = orow['obj_ind'] + 1
         masks.append(mask_img)
 
+        for map_name in params.maps_names:
+            map_fpath = get_img_fpath(scene_path, img_id_str, map_name, 'png')
+            map_ = imread(map_fpath)
+            maps[map_name].append(map_)
+
     if params.aug_name is not None:
         aug = get_aug(params.aug_name)
         imgs = aug(images=imgs)
 
-    res = GtImgsMasks(params.irows, params.orows, imgs, masks)
+    res = GtImgsMasks(params.irows, params.orows, imgs, masks, maps)
     if params.out_size is not None:
         res.crop(params.out_size)
 
@@ -264,12 +314,24 @@ class BopView:
     def __len__(self):
         return len(self.ids)
 
-    def get_gt_task_params(self, i: IdsType, n: Optional[int] = None) -> ImgsObjsGtParams:
+    def _get_img_obj_rows(self, inds: IndsList, is_ids: bool = False) -> tuple[pd.DataFrame, pd.DataFrame, Optional[int]]:
         raise Exception('Unimplemented')
 
-    def get_gt_imgs_masks(self, i: IdsType, n: int) -> GtImgsMasks:
-        params = self.get_gt_task_params(i, n)
-        params.aug_name = self.aug_name
+    def get_gt_task_params(self, i: IdsType, n: Optional[int] = None, is_ids: bool = False) -> ImgsObjsGtParams:
+        if type(i) in (list, np.ndarray, slice):
+            assert n is None
+            inds = i
+        else:
+            assert n is not None and n > 0
+            inds = slice(i, i + n) if not is_ids else np.arange(i, i + n)
+        if is_ids:
+            inds = np.array(inds)
+        irows, orows, out_size = self._get_img_obj_rows(inds, is_ids)
+        params = ImgsObjsGtParams(self.ds.get_ds_path(), irows, orows, out_size, self.aug_name, self.ds.maps_names)
+        return params
+
+    def get_gt_imgs_masks(self, i: IdsType, n: Optional[int] = None, is_ids: bool = False) -> GtImgsMasks:
+        params = self.get_gt_task_params(i, n, is_ids)
         return load_imgs_objs_gt(params)
 
     def get_gt_batch(self, i_batch: int) -> GtImgsMasks:
@@ -333,18 +395,12 @@ class BopImgsView(BopView):
     def __init__(self, ds: 'BopDataset', ids: np.ndarray, batch_size: Optional[int] = None, aug_name: Optional[str] = None):
         super().__init__(ds, ids, batch_size, aug_name)
 
-    def get_gt_task_params(self, i: IdsType, n: Optional[int] = None) -> ImgsObjsGtParams:
-        if type(i) in (list, np.ndarray, range):
-            assert n is None
-            inds = i
-        else:
-            assert n is not None and n > 0
-            inds = slice(i, i + n)
-        img_ds_ids = self.ids[inds]
+    def _get_img_obj_rows(self, inds: IndsList, is_ids: bool = False) -> tuple[pd.DataFrame, pd.DataFrame, Optional[int]]:
+        img_ds_ids = inds if is_ids else self.ids[inds]
         dfi, dfo = self.ds.df_img, self.ds.df_obj
         irows = dfi.loc[img_ds_ids]
         orows = dfo[dfo['img_ds_id'].isin(img_ds_ids)]
-        return ImgsObjsGtParams(self.ds.get_ds_path(), irows, orows)
+        return irows, orows, None
 
 
 class BopObjsView(BopView):
@@ -384,17 +440,19 @@ class BopObjsView(BopView):
 
         if min_mask_ratio is not None or min_bbox_dim_ratio is not None:
             dfo = ds.df_obj.loc[ids]
-            dfo = dfo[dfo['px_count_visib'] > 0]
+            dfo = dfo[dfo['px_count_visib'] > 1e-6]
             dfi = ds.df_img.loc[dfo['img_ds_id']]
             if min_mask_ratio is not None:
                 img_area = dfi['img_width'] * dfi['img_height']
                 inds = dfo['px_count_visib'].values / img_area.values >= min_mask_ratio
+                print(f'min_mask_ratio: {len(inds)} --> {inds.sum()}')
                 dfo, dfi = dfo[inds], dfi[inds]
             if min_bbox_dim_ratio is not None:
                 bbox_ltwh = np.stack(dfo['bbox_visib_ltwh'])
                 bbox_sz = bbox_ltwh[:, 2:].min(axis=1)
                 img_area = dfi[['img_width', 'img_height']].max(axis=1)
                 inds = bbox_sz / img_area.values >= min_bbox_dim_ratio
+                print(f'min_bbox_dim_ratio: {len(inds)} --> {inds.sum()}')
                 dfo = dfo[inds]
             ids = dfo.index.values
 
@@ -407,18 +465,12 @@ class BopObjsView(BopView):
     def set_out_size(self, out_size: int):
         self.out_size = out_size
 
-    def get_gt_task_params(self, i: IdsType, n: Optional[int] = None) -> ImgsObjsGtParams:
-        if type(i) in (list, np.ndarray, range):
-            assert n is None
-            inds = i
-        else:
-            assert n is not None and n > 0
-            inds = slice(i, i + n)
-        obj_ds_ids = self.ids[inds]
+    def _get_img_obj_rows(self, inds: IndsList, is_ids: bool = False) -> tuple[pd.DataFrame, pd.DataFrame, Optional[int]]:
+        obj_ds_ids = inds if is_ids else self.ids[inds]
         orows = self.ds.df_obj.loc[obj_ds_ids]
         img_ds_ids = pd.unique(orows['img_ds_id'])
         irows = self.ds.df_img.loc[img_ds_ids]
-        return ImgsObjsGtParams(self.ds.get_ds_path(), irows, orows, self.out_size)
+        return irows, orows, self.out_size
 
 
 class BopDataset:
@@ -436,17 +488,20 @@ class BopDataset:
     df_img: pd.DataFrame
     df_obj: pd.DataFrame
     shuffled: bool
+    maps_names: list[str]
 
     pool: Optional[mpr.Pool] = None
     pool_refcount = 0
 
-    def __init__(self, bop_path: Path, ds_name: str, ds_subdir: str, df_img: pd.DataFrame, df_obj: pd.DataFrame, shuffled: bool) -> None:
+    def __init__(self, bop_path: Path, ds_name: str, ds_subdir: str, df_img: pd.DataFrame, df_obj: pd.DataFrame, shuffled: bool,
+                 maps_names: tuple[str] = ('norm', 'noc')) -> None:
         self.bop_path = bop_path
         self.ds_name = ds_name
         self.ds_subdir = ds_subdir
         self.df_img = df_img
         self.df_obj = df_obj
         self.shuffled = shuffled
+        self.maps_names = list(maps_names)
     
     @classmethod
     def get_cache_path(cls, bop_path: Path, ds_name: str) -> Path:
@@ -536,7 +591,8 @@ class BopDataset:
             return None
 
     @classmethod
-    def from_dir(cls, bop_path: Path, ds_name: str, ds_subdir: str, shuffle: bool, skip_cache: bool = False) -> 'BopDataset':
+    def from_dir(cls, bop_path: Path, ds_name: str, ds_subdir: str, shuffle: bool, skip_cache: bool = False,
+                 maps_names: tuple[str] = ('norm', 'noc')) -> 'BopDataset':
         ds_path = bop_path / ds_name
         train_path = ds_path / ds_subdir
         cache_path = cls.get_cache_path(bop_path, ds_name)
@@ -547,10 +603,12 @@ class BopDataset:
         ds = None
         if cache_path.exists():
             ds = cls.read_cache(cache_path)
+            if ds is not None:
+                if shuffle:
+                    ds.shuffle()
+                ds.maps_names = maps_names
+                return ds
         
-        if ds is not None:
-            return ds
-
         img_data = []
         img_keys = [
             'img_ds_id',
@@ -633,7 +691,7 @@ class BopDataset:
         df_img.set_index('img_ds_id', inplace=True)
         df_obj.set_index('obj_ds_id', inplace=True)
 
-        ds = BopDataset(bop_path, ds_name, ds_subdir, df_img, df_obj, False)
+        ds = BopDataset(bop_path, ds_name, ds_subdir, df_img, df_obj, shuffled=False, maps_names=maps_names)
         written_to_cache = False
         if shuffle:
             written_to_cache = ds.shuffle()

sdp/lib3d/io.py
@@ -7,18 +7,20 @@ import open3d as o3d
 
 class Mesh:
     mesh_o3d: o3d.geometry.TriangleMesh
+    mul_to_meters: float = 1.0
     _vertices: Optional[np.ndarray] = None
     _triangles: Optional[np.ndarray] = None
     _triangle_normals: Optional[np.ndarray] = None
     _vertex_normals: Optional[np.ndarray] = None
 
-    def __init__(self, mesh_o3d: o3d.geometry.TriangleMesh):
+    def __init__(self, mesh_o3d: o3d.geometry.TriangleMesh, mul_to_meters: float = 1.0):
         self.mesh_o3d = mesh_o3d
+        self.mul_to_meters = mul_to_meters
 
     @property
     def vertices(self) -> np.ndarray:
         if self._vertices is None:
-            self._vertices = np.asarray(self.mesh_o3d.vertices)
+            self._vertices = np.asarray(self.mesh_o3d.vertices) * self.mul_to_meters
         return self._vertices
 
     @property
@@ -42,8 +44,8 @@ class Mesh:
         return self._triangle_normals
 
     @staticmethod
-    def read_mesh(fpath: Path) -> 'Mesh':
+    def read_mesh(fpath: Path, mul_to_meters: float = 1.0) -> 'Mesh':
         mesh_o3d = o3d.io.read_triangle_mesh(str(fpath))
-        return Mesh(mesh_o3d)
+        return Mesh(mesh_o3d, mul_to_meters)
 
 

sdp/lib3d/renderer.py
@@ -44,6 +44,45 @@ def calc_proj_mat(width: int, height: int, focus: float, n: float = 0.01, f: flo
     return res.astype(np.float32)
 
 
+def calc_proj_mat_2(width: int, height: int, cam_mat: np.ndarray, n: float = 0.01, f: float = 10.0) \
+        -> tuple[tuple[int, int], tuple[int, int], np.ndarray]:
+    fx, fy = cam_mat[0, 0], cam_mat[1, 1]
+    cx, cy = cam_mat[:2, 2]
+    cxx, cyy = width - cx, height - cy
+    if cx > cxx:
+        ww = np.ceil(2 * cx).astype(int)
+        off_x = 0
+    else:
+        ww = np.ceil(2 * cxx).astype(int)
+        off_x = ww - width
+    if cy > cyy:
+        hh = np.ceil(2 * cy).astype(int)
+        off_y = 0
+    else:
+        hh = np.ceil(2 * cyy).astype(int)
+        off_y = hh - height
+
+    rx, ry = n / fx, n / fy
+    l, r = -ww / 2 * rx, ww / 2 * rx
+    b, t = -hh / 2 * ry, hh / 2 * ry
+    res = np.array([
+        [2 * n / (r - l), 0, (r + l) / (r - l), 0],
+        [0, 2 * n / (t - b), (t + b) / (t - b), 0],
+        [0, 0, -(f + n) / (f - n), -2 * f * n / (f - n)],
+        [0, 0, -1, 0],
+    ])
+    return (off_x, off_y), (ww, hh), res.astype(np.float32)
+
+
+def all_close_opt(a: Optional[np.ndarray], b: Optional[np.ndarray]) -> bool:
+    an, bn = a is None, b is None
+    if an:
+        return bn
+    if bn:
+        return False
+    return np.allclose(a, b)
+
+
 class ProgContainer:
     def __init__(self):
         self.vertex_shader = shaders.compileShader("""
@@ -199,10 +238,11 @@ class MeshObj:
 
 
 class Renderer:
-    width: int
-    height: int
+    img_size: Tuple[int, int]
     title: str
     hide_window: bool
+    proj_off: tuple[int, int]
+    proj_size: tuple[int, int]
     proj_mat: np.ndarray
     cam_mat: np.ndarray
     window: Any
@@ -210,17 +250,19 @@ class Renderer:
     mesh_objs: Dict[Any, MeshObj]
     cv_to_opengl_mat: np.ndarray
 
-    def __init__(self, meshes: Dict[Any, Mesh], win_size: Tuple[int, int] = (640, 480), title: str = 'Renderer',
+    def __init__(self, meshes: Dict[Any, Mesh], img_size: Tuple[int, int] = (640, 480), title: str = 'Renderer',
                  hide_window: bool = False):
-        self.width, self.height = win_size
-        self.title = title
-        self.hide_window = hide_window
-        self.proj_mat = calc_proj_mat(self.width, self.height, self.width)
+        self.img_size = img_size
+        # self.cam_mat = np.empty((3, 3), dtype=float)
         self.cam_mat = np.array([
-            self.width, 0, self.width / 2,
-            0, self.width, self.height / 2,
+            self.img_size[0], 0, self.img_size[0] / 2,
+            0, self.img_size[1], self.img_size[1] / 2,
             0, 0, 1.,
         ]).reshape((3, 3))
+        self.proj_off = 0
+        self.proj_size = self.img_size
+        self.title = title
+        self.hide_window = hide_window
 
         if not glfw.init():
             raise Exception(f'Error. Cannot init GLFW!')
@@ -230,9 +272,9 @@ class Renderer:
         glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
         glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
 
-        self.window = glfw.create_window(self.width, self.height, self.title, None, None)
+        self.window = glfw.create_window(self.img_size[0], self.img_size[1], self.title, None, None)
         if not self.window:
-            raise Exception(f'Error. Cannot create window of size: {self.width}x{self.height}')
+            raise Exception(f'Error. Cannot create window of size: {self.img_size}')
         glfw.make_context_current(self.window)
         if self.hide_window:
             glfw.hide_window(self.window)
@@ -264,36 +306,28 @@ class Renderer:
     def create_mesh_objs(meshes: Dict[Any, Mesh], prog: ProgContainer) -> Dict[Any, MeshObj]:
         return {mid: MeshObj(mesh.vertices, mesh.vertex_normals, mesh.triangles, prog) for mid, mesh in meshes.items()}
 
-    def set_window_size(self, win_size: Tuple[int, int]):
-        width, height = win_size
-        if (self.width, self.height) == (width, height):
+    def set_intrinsics(self, img_size: Optional[Tuple[int, int]] = None, cam_mat: Optional[np.ndarray] = None):
+        if img_size is None:
+            img_size = self.img_size
+        if cam_mat is None:
+            cam_mat = self.cam_mat
+        if self.img_size == img_size and all_close_opt(self.cam_mat, cam_mat):
             return
-        self.width, self.height = width, height
-        self.proj_mat = calc_proj_mat(self.width, self.height, self.cam_mat[0, 0])
-        glfw.set_window_size(self.window, self.width, self.height)
-        glViewport(0, 0, self.width, self.height)
+
+        self.img_size = img_size
+        self.cam_mat = cam_mat
+        self.proj_off, self.proj_size, self.proj_mat = \
+            calc_proj_mat_2(self.img_size[0], self.img_size[1], self.cam_mat)
+
+        glfw.set_window_size(self.window, *self.proj_size)
+        # glViewport(0, 0, *self.proj_size)
         for _ in range(2):
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             glfw.swap_buffers(self.window)
 
-    def set_camera_matrix(self, cam_mat: np.ndarray):
-        if self.cam_mat is not None and np.allclose(self.cam_mat, cam_mat):
-            return
-        self.cam_mat = cam_mat.astype(np.float32)
-        self.proj_mat = calc_proj_mat(self.width, self.height, self.cam_mat[0, 0])
-        # fovy_half_tan = (self.height / 2) / self.cam_mat[1, 1]
-        # fovy = np.arctan(fovy_half_tan) * 2 * (180 / np.pi)
-
-        # print(f'Fov y: {fovy:.2f}')
-        # glMatrixMode(GL_PROJECTION)
-        # glLoadIdentity()
-        # gluPerspective(fovy, self.width / self.height, 0.01, 10)
-        # glMatrixMode(GL_MODELVIEW)
-        # glLoadIdentity()
-
     def gen_colors(self, cam_mat: np.ndarray, objs_poses: List[Tuple[Any, np.ndarray]], out_type: OutputType,
                    obj_color: Optional[Color4i] = None) -> np.ndarray:
-        self.set_camera_matrix(cam_mat)
+        self.set_intrinsics(cam_mat=cam_mat)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -304,12 +338,25 @@ class Renderer:
             mesh_obj.draw(self.proj_mat, obj_cam_mat, out_type, obj_color)
 
         image_type = GL_RGBA if out_type == OutputType.Overlay else GL_RGB
-        width, height = glfw.get_framebuffer_size(self.window)
-        image_buffer = glReadPixels(0, 0, width, height, image_type, GL_UNSIGNED_BYTE)
-        image = np.frombuffer(image_buffer, dtype=np.uint8).reshape((height, width, -1))
-        if width != self.width or height != self.height:
-            image = cv2.resize(image, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        fb_size = glfw.get_framebuffer_size(self.window)
+        # print(f'Frame buffer size: {fb_size}. Proj size: {self.proj_size}. off: {self.proj_off}')
+        image_buffer = glReadPixels(0, 0, fb_size[0], fb_size[1], image_type, GL_UNSIGNED_BYTE)
+        image = np.frombuffer(image_buffer, dtype=np.uint8).reshape((fb_size[1], fb_size[0], -1))
+
+        # On Retina Display fb_size = 2 * self.proj_size
+        # assert fb_size == self.proj_size
+        if fb_size != self.proj_size:
+            assert fb_size[0] // self.proj_size[0] == fb_size[1] // self.proj_size[1],\
+                f'Frame buffer size {fb_size} is not divided by proj_size {self.proj_size} with a constant'
+            image = cv2.resize(image, self.proj_size, interpolation=cv2.INTER_AREA)
+
+        off_x, off_y = self.proj_off
         image = cv2.flip(image, 0)
+        if off_x > 0 or off_y > 0 or self.img_size != self.proj_size:
+            # print(f'Extracting proj_size {self.img_size}, proj_off {self.proj_off} from image of '
+            #       f'shape: {image.shape[1], image.shape[0]}')
+            w, h = self.img_size
+            image = image[off_y:off_y + h, off_x:off_x + w]
 
         glfw.swap_buffers(self.window)
 
@@ -332,23 +379,23 @@ def gen_rot_vec() -> Tuple[np.ndarray, float]:
 def test_renderer():
     data_path = Path(os.path.expandvars('$HOME/data'))
     # data_path = Path(os.path.expandvars('/ws/data'))
-    obj_path = data_path / 'sds_data/objs/teamug.stl'
-    # obj_path = data_path / 'sds/itodd/models/obj_000004.ply'
+    # obj_path, mul_to_meters = data_path / 'sds_data/objs/teamug.stl', 1.0
+    obj_path, mul_to_meters = data_path / 'bop/itodd/models/obj_000004.ply', 1e-3
     print(f'Loading {obj_path}')
-    mesh = Mesh.read_mesh(obj_path)
+    mesh = Mesh.read_mesh(obj_path, mul_to_meters)
     mesh_id = 'teamug'
     meshes = {mesh_id: mesh}
 
     w, h = 640, 480
     w, h = 2048, 2048
     cam_mat = np.array([
-        w / 2, 0., w / 2,
-        0, w / 2, h / 2,
+        w / 2, 0., w / 2 - 10,
+        0, w / 2, h / 2 + 10,
         0, 0, 1,
     ]).reshape((3, 3))
 
     ren = Renderer(meshes, (w, h), hide_window=True)
-    ren.set_camera_matrix(cam_mat)
+    ren.set_intrinsics(cam_mat=cam_mat)
 
     np.random.seed(1)
     pos_center = np.array((0., 0., 1.))